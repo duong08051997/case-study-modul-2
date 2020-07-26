8e9ca89a352951bc14928886e18ffe430b92b4af
duong08051997 <duong08051997@gmai.com>
2020-07-26 14:59:54 +0700

    update

--- src/View/Student/detail.php
+++ src/View/Student/detail.php
@@ -1,43 +1,49 @@
+<div class="container" >
 
-<div class="pt-3"></div>
-<div class=" col-lg-4" id="detail">
+    <div class="pt-3"></div>
+    <div class="row" >
+        <div class=" col-sm-6 " id="detail">
 
-<table class="table  " >
-    <thead class=" ">
-    <?php if (empty($studentss)) : ?>
-        <tr>
-            <td>No data</td>
-        </tr>
-    <?php else: ?>
+            <table class="table "  >
+                <thead class=" ">
+                <?php if (empty($studentss)) : ?>
+                    <tr>
+                        <td>No data</td>
+                    </tr>
+                <?php else: ?>
 
-        <tr>
-            <td style="" > Student Name: <?php echo $studentss[0][0] ?> </td>
-        </tr>
-        <tr>
-            <td> Class: <?php echo $studentss[0][1] ?> </td>
-        </tr>
-        <tr>
-            <td> Age: <?php echo $studentss[0][2] ?> </td>
-        </tr>
-        <tr>
-            <td>Gender: <?php echo $studentss[0][3] ?></td>
-        </tr>
-        <tr>
-            <td>Address: <?php echo $studentss[0][4] ?> </td>
-        </tr>
-        </thead>
-    </table>
-             </div>
-    <!--        --><?php //foreach ($   studentss as $key => $student): ?>
-                          <!--            <tr>-->
-        <!--                <td>--><?php //echo $student[5] ?><!--</td>-->
-        <!--            </tr>-->
-        <!--            <tr>-->
-        <!--                <td>--><?php //echo $student["score"] ?><!--</td>-->
-        <!--            </tr>-->
-        <!--        --><?php //endforeach; ?>
-        <!---->
-    <?php endif; ?>
-<!--    </thead>-->
-<!--</table>-->
-<!--                 </div>    -->
+                <tr>
+                    <td style="border: none"> Student Name: <?php echo $studentss[0][0] ?> </td>
+                </tr>
+                <tr>
+                    <td style="border: none"> Class: <?php echo $studentss[0][1] ?> </td>
+                </tr>
+                <tr>
+                    <td style="border: none"> Age: <?php echo $studentss[0][2] ?> </td>
+                </tr>
+                <tr>
+                    <td style="border: none">Gender: <?php echo $studentss[0][3] ?></td>
+                </tr>
+                <tr>
+                    <td style="border: none">Address: <?php echo $studentss[0][4] ?> </td>
+                </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="pt-3"></div>
+        <div class=" col-sm-6 " id="detail">
+            <table class="table  ">
+                <thead class=" ">
+                <?php foreach ($studentss as $key => $student): ?>
+                    <tr>
+                        <td style="border: none"><?php echo $student[5] ?></td>
+                        <td style="border: none"><?php echo $student["score"] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+
+                <?php endif; ?>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
